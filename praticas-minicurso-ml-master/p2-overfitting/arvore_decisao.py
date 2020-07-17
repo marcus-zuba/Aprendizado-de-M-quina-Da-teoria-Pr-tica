b4442d4b1d2170cb428edb3229db75006322e9af
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2020-07-17 18:51:43 -0300

    Adding exercises about Overfitting and Underfitting

--- praticas-minicurso-ml-master/p2-overfitting/arvore_decisao.py
+++ praticas-minicurso-ml-master/p2-overfitting/arvore_decisao.py
@@ -2,7 +2,7 @@ from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from typing import Tuple
+from typing import Tuple, List
 def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     """
         Retorna o modelo a ser usado.
@@ -16,10 +16,10 @@ def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     #instancia a arvore de decisão (use  a classe DecisionTreeClassifier do Scikitlearn, defina o
     #..parametro min_samples_split e random_state=1 - é muito importante manter a seed fixa para
     #..que os sejam resultados mantenham sempre o mesmo - reprodutibilidade)
-    decision_tree = None
+    decision_tree = DecisionTreeClassifier(min_samples_split=min_samples_split, random_state=1)
 
     #retone o modelo por meio do método fit
-    return None
+    return decision_tree.fit(X,y)
 
 def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd.DataFrame,pd.DataFrame]:
     """
@@ -29,16 +29,16 @@ def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd
         Retorna uma tupla com o treino e teste separados
     """
     #1. obtenha o treino usando o método sample do DataFrame
-    df_treino = None
+    df_treino = df.sample(frac=val_proporcao_treino, random_state=1)
 
     #2. Para obter o teste, selecione as instancias que estão em df e não estão em df_treino (use o método drop)
-    df_teste = None
+    df_teste = df.drop(df_treino.index)
 
     return df_treino,df_teste
 
 
 
-def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.DataFrame, y_teste:pd.Series, min_samples_split:float) -> Tupe[List[float],float]:
+def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.DataFrame, y_teste:pd.Series, min_samples_split:float) -> Tuple[List[float],float]:
     """
         Efetua a classificação, retornando:
             - O vetor y_predicted em que, para cada posição i,
@@ -52,13 +52,13 @@ def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.Data
                 * np.sum soma os valores de um vetor (considerando True=1 e False=0)
     """
     #cria o modelo (use a função previamente criada)
-    model_dtree = None
+    model_dtree = cria_modelo(x_treino, y_treino, min_samples_split)
 
     #realiza a predição (use o método predict do modelo)
-    y_predicted = None
+    y_predicted = model_dtree.predict(x_teste)
 
     #calcule a acurácia
-    acuracia = None
+    acuracia = ((y_predicted==y_teste).sum())/len(y_teste)
 
 
     return y_predicted,acuracia
@@ -84,9 +84,9 @@ def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
 
     for min_samples in np.arange(0.001,0.7,0.01):
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no teste
-        y_predicted, ac_teste = None
+        y_predicted, ac_teste = faz_classificacao(X_treino, y_treino, X_teste,y_teste, min_samples)
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no treino
-        y_predicted, ac_treino = None
+        y_predicted, ac_treino = faz_classificacao(X_treino, y_treino, X_treino,y_treino, min_samples)
 
         #adiciona a acuracia no treino, no teste e o parametro min_samples
         arr_ac_treino.append(ac_treino)
@@ -94,5 +94,12 @@ def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
         arr_min_samples.append(min_samples)
 
     #plota o resultado
-    plt.plot(arr_min_samples,arr_ac_treino,"b--")
-    plt.plot(arr_min_samples,arr_ac_teste,"r-")
+    plt.xlabel('min_sample')
+    plt.ylabel('Acurácia')
+    plt.grid(True)
+    plt.plot(arr_min_samples,arr_ac_treino,"b--", label="Treino")
+    plt.plot(arr_min_samples,arr_ac_teste,"r-", label="Teste")
+    plt.legend(loc='upper right', frameon=True)
+
+    #coloca a legenda
+    
